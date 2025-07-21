2bed49ef542f5146fc80471b2cd76490349983eb
Abir Saha <154303202+Abir2307@users.noreply.github.com>
2025-07-22 00:44:02 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: 'https://todo-client-lovat.vercel.app/',
+  origin: 'https://todo-client-lovat.vercel.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   });
   await app.listen(process.env.PORT || 3001);
