acefebb8f971fd7c8a592bf061a219897e4a2b37
Abir Saha <154303202+Abir2307@users.noreply.github.com>
2025-07-22 00:47:11 +0530

    Initial commit for NestJS Todo API

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/defaultdb'),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/todo-list'),
     TodosModule,],
   controllers: [AppController],
   providers: [AppService],
